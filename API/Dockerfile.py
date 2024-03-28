ccd25bcd269a85e0ff4ff83029369ac80bbbe134
Nasrin <esmailian89@yahoo.com>
2024-03-28 23:35:55 +0100

    update

--- API/Dockerfile.py
+++ API/Dockerfile.py
@@ -1,10 +1,19 @@
-FROM python:3.11.8
-WORKDIR /api
-COPY api/. /api
-COPY /api/XGBoost_artifacts.pkl/. /api/XGBoost_artifacts.pkl
-COPY api/app.py /api/app.py
-COPY requirements.txt /api
+# Use an official Python runtime as a parent image
+FROM python:3.11-slim
+
+RUN mkdir /app
+
+
+# Set the working directory to /app
+WORKDIR /app
+
+COPY . /app
+
+# Update pip
+RUN pip install --upgrade pip
+
+# Install dependencies from "requirements.txt"
 RUN pip install -r requirements.txt
-ENTRYPOINT ["uvicorn"]
-CMD "api.app:app", "--host", "0.0.0.0", "--port", "8000"
-EXPOSE 8000
+
+# Command to run your application using uvicorn
+CMD uvicorn --host 0.0.0.0 --port 8000 app:app
