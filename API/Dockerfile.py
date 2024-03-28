6c1eda98be176ba21c274213774d5971cabc0b12
Nasrin <esmailian89@yahoo.com>
2024-03-28 23:53:12 +0100

    update

--- API/Dockerfile.py
+++ API/Dockerfile.py
@@ -1,20 +1,9 @@
-# Use an official Python runtime as a parent image
-FROM python:3.9-slim
-
-# Set the working directory in the container
-WORKDIR ./app
-
-# Copy the current directory contents into the container at /usr/src/app
-COPY . .
-
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir fastapi uvicorn joblib numpy pandas scikit-learn
-
-# Make port 8000 available to the world outside this container
+FROM python:3.12.1
+WORKDIR /api
+COPY api/. /api
+COPY api/app.py /api/app.py
+COPY requirements.txt /api
+RUN pip install -r requirements.txt
+ENTRYPOINT ["uvicorn"]
+CMD uvicorn --host 0.0.0.0 --port 8000 app:app
 EXPOSE 8000
-
-# Define environment variable
-ENV MODEL_PATH= ./API/XGBoost_artifacts.pkl
-
-# Run app.py when the container launches
-CMD ["uvicorn", "api.app:app", "--host", "0.0.0.0", "--port", "8000"]
