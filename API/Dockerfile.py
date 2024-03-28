8580574642fcd833f71c43dcc1010c0cfe48105c
Nasrin <esmailian89@yahoo.com>
2024-03-29 00:05:17 +0100

    update

--- API/Dockerfile.py
+++ API/Dockerfile.py
@@ -1,9 +1,23 @@
+# Use an official Python runtime as a parent image
 FROM python:3.12.1
+
+# Set the working directory in the container
 WORKDIR /api
-COPY api/. /api
-COPY api/app.py /api/app.py
+
+# Copy the current directory contents into the container at /api
+COPY . /api
+
+# Copy requirements.txt and install the Python dependencies
 COPY requirements.txt /api
 RUN pip install -r requirements.txt
-ENTRYPOINT ["uvicorn"]
-CMD uvicorn --host 0.0.0.0 --port 8000 api.app:app
+
+# Make port 8000 available to the world outside this container
 EXPOSE 8000
+
+# Define environment variable to hold the command to run Uvicorn
+ENV UVICORN_CMD="uvicorn --host 0.0.0.0 --port 8000 api.app:app"
+
+# Run Uvicorn when the container launches
+ENTRYPOINT ["sh", "-c", "$UVICORN_CMD"]
+
+
